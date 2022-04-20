f54acbe6b1c0df618ea56d96577b0a2b79a504b4
Dmitriy <bmd4work@gmail.com>
2022-04-20 23:55:36 +0600

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './components/About/About';
+import Gallery from './components/Gallery/Gallery';
+import Navigation from './components/Navigation/Nav';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Gallery />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation/Nav.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <Nav variant="pills" defaultActiveKey="1">
+      <Nav.Item>
+        <Nav.Link as={Link} to="/" eventKey="1">
+          Gallery
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link as={Link} to="/about" eventKey="2">
+          About
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
+
+export default Navigation;
