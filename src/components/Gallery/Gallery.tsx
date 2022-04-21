83524bfdbda9c69ee0bb125829ac4b255bba8afb
Dmitriy <bmd4work@gmail.com>
2022-04-21 16:19:15 +0600

    feat: add categories

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
+
+const categories = ['Все', 'Категория 1', 'Категория 2', 'Категория 3', 'Категория 4'];
 
 function Gallery() {
   return (
     <section className="gallery">
       <ListGroup horizontal>
-        <ListGroup.Item>This</ListGroup.Item>
-        <ListGroup.Item>ListGroup</ListGroup.Item>
-        <ListGroup.Item>renders</ListGroup.Item>
-        <ListGroup.Item>horizontally!</ListGroup.Item>
+        {categories.map((cat, index) => {
+          return (
+            <ListGroup.Item key={index} variant="primary">
+              <Button variant="light">{cat}</Button>
+            </ListGroup.Item>
+          );
+        })}
       </ListGroup>
     </section>
   );
--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
 import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <Nav variant="pills" defaultActiveKey="1" className="justify-content-end">
-      <Nav.Item>
-        <Nav.Link as={Link} to="/" eventKey="1">
-          Gallery
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/about" eventKey="2">
-          About
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Navbar bg="primary" variant="dark" expand={false} collapseOnSelect={true} className="mb-3">
+      <Container fluid>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="start">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">Закрыть</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link as={Link} to="/" eventKey="1">
+                Галерея
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about" eventKey="2">
+                Обо мне
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+        <Navbar.Brand>
+          <Nav.Link as={Link} to="/" eventKey="1">
+            Добро пожаловать на сайт-галерею
+          </Nav.Link>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 }
 
